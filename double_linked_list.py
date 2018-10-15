1fa900b9b88ec5b97a32a70da49382e2ea1a008b
E.J. Bryant <ejbryant28@gmail.com>
2018-10-15 23:18:07 +0000

    added tests

--- double_linked_list.py
+++ double_linked_list.py
@@ -77,10 +77,50 @@ class LinkedList():
 		node = self.search(data)
 
 		previous = node.prev
-		previous = node.next
+
+		previous.next = node.next
 		node.next = None
 		node.prev = None
 
 		self.length -= 1
 
 		return node
+
+linked = LinkedList()
+assert(linked.head == None)
+assert(linked.length == 0)
+assert(linked.tail == None)
+empty_list = linked.search('thing')
+assert(empty_list == 'This is an empty list')
+
+linked.append('first')
+assert(linked.head.data == 'first')
+assert(linked.tail.data == 'first')
+assert(linked.length == 1)
+
+linked.append('second')
+assert(linked.head.data == 'first')
+assert(linked.head.next.data == 'second')
+assert(linked.tail.data == 'second')
+assert(linked.length == 2)
+
+
+not_found = linked.search('third')
+assert(not_found == 'not in list')
+
+search = linked.search('first')
+assert(search.data == 'first')
+
+linked.append('third')
+assert(linked.length == 3)
+last = linked.remove_last()
+assert(last.data == 'third')
+assert(linked.length == 2)
+
+linked.append('third')
+assert(linked.length == 3)
+mid = linked.remove_middle('second')
+assert(linked.length == 2)
+assert(linked.head.next.data == 'third')
+
+print('passed!')
