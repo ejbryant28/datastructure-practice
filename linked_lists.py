d17b5a7ed8cab4358c2faf9c92ca158010c67124
E.J. Bryant <ejbryant28@gmail.com>
2018-10-15 18:26:28 +0000

    completed singly linked list class

---
+++ linked_lists.py
@@ -0,0 +1,87 @@
+class Node(object):
+
+	def __init__(self, data):
+
+		self.data = data
+		self.next = None
+
+	def __repr__(self):
+		return "<Node: {}>".format(self.node)
+
+class LinkedList(Node):
+
+	def __init(self):
+		
+		self.head = None
+		self.length = 0
+
+	def _find_tail(self):
+
+		tail = False
+		current = self.head
+
+		while not tail:
+			if current.next == None:
+				tail = current
+
+			else: 
+				current = current.next
+
+		return tail
+
+	def _find_penultimate(self):
+
+		penult = None
+		current = self.head
+
+		while not penult:
+			if current.next.next == None:
+				penult = current
+
+			else:
+				current = current.next
+		return penult
+
+
+	def append(self, data)
+		new_node = Node(data)
+		self.length += 1
+		if self.head == None:
+			self.head = new_node
+			return 'completed'
+
+		#find the tail, set the tail's .next to be new_node
+		tail = _find_tail()
+		tail.next = new_node
+		return 'completed'
+		
+
+	def search(self, data):
+
+		if self.head == None:
+			return 'This is an empty list'
+
+		current = self.head
+		found = False
+
+		while not found:
+			if current.data == data:
+				found = True
+			elif current.next == None:
+				return 'not in list'
+			current = current.next
+
+		return current
+
+	def remove_last(self):
+
+		pen_ultimate = _find_penultimate()
+
+		tail = pen_ultimate.next
+		pen_ultimate.next = None
+
+		return tail
+		
+
+	def remove_middle(self, data):
+		pass
