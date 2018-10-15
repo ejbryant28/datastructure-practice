c4fdc9774a76209fac673336c1f258884f1d4377
E.J. Bryant <ejbryant28@gmail.com>
2018-10-15 22:57:43 +0000

    added tests

--- linked_lists.py
+++ linked_lists.py
@@ -6,11 +6,16 @@ class Node(object):
 		self.next = None
 
 	def __repr__(self):
-		return "<Node: {}>".format(self.node)
+		return "<Node: {}>".format(self.data)
 
-class LinkedList(Node):
+test = Node('test')
+assert(test.data=='test')
+assert(test.next == None)
 
-	def __init(self):
+
+class LinkedList():
+
+	def __init__(self):
 		
 		self.head = None
 		self.length = 0
@@ -43,7 +48,7 @@ class LinkedList(Node):
 		return penult
 
 
-	def append(self, data)
+	def append(self, data):
 		new_node = Node(data)
 		self.length += 1
 		if self.head == None:
@@ -51,12 +56,12 @@ class LinkedList(Node):
 			return 'completed'
 
 		#find the tail, set the tail's .next to be new_node
-		tail = _find_tail()
+		tail = self._find_tail()
 		tail.next = new_node
 		return 'completed'
 		
 
-	def search(self, data):
+	def search(self, search):
 
 		if self.head == None:
 			return 'This is an empty list'
@@ -65,17 +70,18 @@ class LinkedList(Node):
 		found = False
 
 		while not found:
-			if current.data == data:
+			if current.data == search:
 				found = True
 			elif current.next == None:
 				return 'not in list'
-			current = current.next
+			else:
+				current = current.next
 
 		return current
 
 	def remove_last(self):
 
-		pen_ultimate = _find_penultimate()
+		pen_ultimate = self._find_penultimate()
 
 		tail = pen_ultimate.next
 		pen_ultimate.next = None
@@ -87,10 +93,40 @@ class LinkedList(Node):
 
 	def remove_middle(self, data):
 		
-		prev = _find_penultimate(data)
+		prev = self._find_penultimate(data)
 		search = prev.next
 
 		prev.next = search.next
 		search.next = None
 
 		return search
+
+linked = LinkedList()
+assert(linked.head == None)
+assert(linked.length == 0)
+empty_list = linked.search('thing')
+assert(empty_list == 'This is an empty list')
+
+linked.append('first')
+assert(linked.head.data == 'first')
+assert(linked.length == 1)
+
+linked.append('second')
+assert(linked.head.data == 'first')
+assert(linked.head.next.data == 'second')
+assert(linked.length == 2)
+
+
+not_found = linked.search('third')
+assert(not_found == 'not in list')
+
+search = linked.search('first')
+assert(search.data == 'first')
+
+linked.append('third')
+assert(linked.length == 3)
+last = linked.remove_last()
+assert(last.data == 'third')
+assert(linked.length == 2)
+
+print('passed!')
